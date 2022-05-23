a115e91af1d675c2748a8b22437c17f33db7c7c2
Instelce <intelce.developpement@gmail.com>
2022-05-23 19:04:02 +0200

    Start the frontend

--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'frontend/home.html')
+
+def login(request):
+    return render(request, 'frontend/login.html', {'title': 'Login'})
+
+def register(request):
+    return render(request, 'frontend/register.html', {'title': 'Sign Up'})
