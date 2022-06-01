9ee7a4f4f1d522e2d6057f87c39dc6ee7059228f
Instelce <instelce@protonmail.com>
2022-06-01 20:13:36 +0200

    Add form and frontend of register

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login'),
+    path('download', views.download, name='download'),
+    path('login', views.Login.as_view(template_name='frontend/login.html', extra_context={'title': 'Login'}), name='login'),
     path('register', views.register, name='register')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
+
+from .forms import *
 
 
 def home(request):
     return render(request, 'frontend/home.html')
 
-def login(request):
-    return render(request, 'frontend/login.html', {'title': 'Login'})
+def download(request):
+    return render(request, 'frontend/download.html')
 
 def register(request):
-    return render(request, 'frontend/register.html', {'title': 'Sign Up'})
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'{username} account has been created !')
+            return redirect('download')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'frontend/register.html', {'form': form, 'title': 'Sign Up'})
+
+
+class Login(LoginView):
+    def get_success_url(self):
+        user = self.request.user
+        return reverse('home')
